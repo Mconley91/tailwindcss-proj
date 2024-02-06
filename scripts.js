564e9f82bc8e62b0c4f959a06bba93b617d0f85e
Matt Conley <matthew.e.conley@gmail.com>
2024-02-06 17:32:53 -0500

    added basic calculator functionality with addition, subtraction, multiplaction, and division capabilities. Can currently only compute two numbers at a time.

--- scripts.js
+++ scripts.js
@@ -1,4 +1,14 @@
+let readyToAdd = false;
+let readyToSubtract = false;
+let readyToMultiply = false;
+let readyToDivide = false;
+let resultGot = false;
+
 function numKey(key){
+    if (resultGot === true){
+        document.getElementById('readout').innerHTML = '';
+        resultGot = false;
+    };
     document.getElementById('readout').innerHTML += key
 };
 function backSpace(){
@@ -8,4 +18,51 @@ function backSpace(){
 };
 function clearReadout(){
     document.getElementById('readout').innerHTML = "";
+    sessionStorage.clear();
+};
+function additionButton(){
+        sessionStorage.setItem('firstNumber',document.getElementById('readout').innerHTML);
+        document.getElementById('readout').innerHTML = '';
+        readyToAdd = true;
+};
+function subtractionButton(){
+    sessionStorage.setItem('firstNumber',document.getElementById('readout').innerHTML);
+    document.getElementById('readout').innerHTML = '';
+    readyToSubtract = true;
+};
+function multiplicationButton(){
+    sessionStorage.setItem('firstNumber',document.getElementById('readout').innerHTML);
+    document.getElementById('readout').innerHTML = '';
+    readyToMultiply = true;
+};
+function divisionButton(){
+    sessionStorage.setItem('firstNumber',document.getElementById('readout').innerHTML);
+    document.getElementById('readout').innerHTML = '';
+    readyToDivide = true;
+};
+function getResult(){
+    if (readyToAdd === true) {
+        let result = parseInt(sessionStorage.getItem('firstNumber')) + parseInt(document.getElementById('readout').innerHTML)
+        document.getElementById('readout').innerHTML = result
+        readyToAdd = false;
+        resultGot = true;
+    }
+    if (readyToSubtract === true) {
+        let result = parseInt(sessionStorage.getItem('firstNumber')) - parseInt(document.getElementById('readout').innerHTML)
+        document.getElementById('readout').innerHTML = result
+        readyToSubtract = false;
+        resultGot = true;
+    }
+    if (readyToMultiply === true) {
+        let result = parseInt(sessionStorage.getItem('firstNumber')) * parseInt(document.getElementById('readout').innerHTML)
+        document.getElementById('readout').innerHTML = result
+        readyToMultiply = false;
+        resultGot = true;
+    }
+    if (readyToDivide === true) {
+        let result = parseInt(sessionStorage.getItem('firstNumber')) / parseInt(document.getElementById('readout').innerHTML)
+        document.getElementById('readout').innerHTML = result
+        readyToDivide = false;
+        resultGot = true;
+    }
 };
